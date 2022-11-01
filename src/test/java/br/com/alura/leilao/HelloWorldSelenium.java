37d61b151e1d83b571bc7b4d3f7cd25b1c9c7a50
Luan Kenzley <luankenzley@hotmail.com>
2022-10-31 22:51:46 -0300

    subindo projeto completo

---
+++ src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -0,0 +1,18 @@
+package br.com.alura.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorldSelenium {
+
+    @Test
+    public void hello() {
+
+        WebDriver browser = new ChromeDriver();
+        browser.navigate().to("localhost:8080/leiloes");
+        browser.quit();
+
+    }
+
+}
